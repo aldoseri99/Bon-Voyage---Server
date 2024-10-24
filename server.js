dc38a7c73c118dd4eb4b76b8816919f39fa08b53
aldoseri99 <aldosariali9@gmail.com>
2024-10-24 23:44:02 +0300

    server modefied

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+const PORT = process.env.PORT || 3000
+
 const db = require('./db')
 const app = express()
 
