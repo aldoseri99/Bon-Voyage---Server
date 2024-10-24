858e7c68afab24fb506b144bcd77466a291b7179
aldoseri99 <aldosariali9@gmail.com>
2024-10-24 23:37:17 +0300

    deploy

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
 
-const PORT = process.env.PORT || 3001
-
-const db = require("./db")
+const db = require('./db')
 const app = express()
 
 app.use(cors())
@@ -12,24 +10,24 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 
-const AuthRouter = require("./routes/AuthRouter")
-app.use("/auth", AuthRouter)
+const AuthRouter = require('./routes/AuthRouter')
+app.use('/auth', AuthRouter)
 
-const PostRouter = require("./routes/PostRouter")
-app.use("/Posts", PostRouter)
+const PostRouter = require('./routes/PostRouter')
+app.use('/Posts', PostRouter)
 
-const ActivitiesRouter = require("./routes/ActivitiesRouter")
-app.use("/activities", ActivitiesRouter)
+const ActivitiesRouter = require('./routes/ActivitiesRouter')
+app.use('/activities', ActivitiesRouter)
 
-const CommentRouter = require("./routes/CommentRouter")
-app.use("/comment", CommentRouter)
+const CommentRouter = require('./routes/CommentRouter')
+app.use('/comment', CommentRouter)
 
-const LocationRouter = require("./routes/LocationRouter")
-app.use("/location", LocationRouter)
+const LocationRouter = require('./routes/LocationRouter')
+app.use('/location', LocationRouter)
 
-app.use("/", (req, res) => {
+app.use('/', (req, res) => {
   res.send(`Connected!`)
 })
 
